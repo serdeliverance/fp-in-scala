9d7cf6603ba324fdcdcbacbcc2f07d9990792ab8
Sergio Cano <serdeliverance@gmail.com>
2020-05-10 12:37:08 -0300

    Option lecture and exercises - part 1

--- src/main/scala/lecture/handlingerrors/HandlingErrorsWithoutExceptions.scala
+++ src/main/scala/lecture/handlingerrors/HandlingErrorsWithoutExceptions.scala
@@ -1,6 +1,5 @@
 package lecture.handlingerrors
 
-
 sealed trait Option[+A] {
 
   def map[B](f: A => B): Option[B] = this match {
@@ -33,20 +32,12 @@ sealed trait Option[+A] {
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
-class HandlingErrorsWithoutExceptions extends App {
-
-}
 
 object HandlingErrorsWithoutExceptions {
 
   def mean(xs: Seq[Double]):Option[Double] = {
     if (xs.isEmpty) None
     else Some(xs.sum/xs.length)
-  }
-
-  // Ex 4.2
-  def variance(xs: Seq[Double]): Option[Double] = {
-    mean(xs) flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
   }
 
   /**
@@ -56,4 +47,44 @@ object HandlingErrorsWithoutExceptions {
    * It is useful to model a computation with multiples stages when any of these can fail. It that case,
    * the computation is abort as soon as the first fail is encountered
    */
+  // Ex 4.2
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs) flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
+  }
+
+  def lift[A,B](f: A => B): Option[A] => Option[B] = _ map f
+
+  def Try[A](a: => A): Option[A] =
+    try Some(a)
+    catch { case e: Exception => None}
+
+  def insuranceRateQuote(age: Int, numberOfSpeedingTickets: Int): Double = ???
+
+  // Ex 4.3: my solution
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A,B) => C): Option[C] = (a, b) match {
+    case a == None || b == None => None
+    case (Some(elemA), Some(elemB)) => Some(f(elemA,elemB))
+  }
+
+  // Ex 4.3: solution from book using flatMap
+  def map2b[A,B,C](a: Option[A], b: Option[B])(f: (A,B) => C): Option[C] =
+    a flatMap (aa => b map (bb => f(aa,bb)))
+
+  def parseInsuranceRateQuote(age: String, numberOfSpeedingTickets: String): Option[Double] = {
+    val optAge = Try(age.toInt)
+    val optTickets = Try(numberOfSpeedingTickets.toInt)
+
+    map2b(optAge, optTickets)(insuranceRateQuote)
+  }
+}
+
+object Playground extends App {
+
+  import HandlingErrorsWithoutExceptions._
+
+  val absO = lift(math.abs)
+
+  val abs0result = absO(Some(-2)).getOrElse(throw new Exception("illegal input"))
+
+  println(abs0result)
 }
